e76c3659f8309e165dcb9530a93c685b1350e7e1
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-08-30 20:26:43 +0100

    basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,11 +5,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
     """basic route"""
     return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
