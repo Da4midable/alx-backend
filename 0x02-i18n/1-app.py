2ae54fbcc49d9af7c9d259ac54feb28ab60e469e
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-08-30 21:17:57 +0100

    using that class as config for your Flask app

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""basic flask app with Babel configuration"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """Sets Babel's default locale ("en") and timezone ("UTC")."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """basic route"""
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
